263b9d8d599ee75d2204a871159b5e56947bcfc7
Tope <tope628@gmail.com>
2017-08-02 06:41:01 +0000

    errors

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        if ord(i) >= ord('a') and ord(i) <= ord('z'):
+            i -= 32
+        print("{:s}".format(chr(ord(i)), end='')
+    print()
