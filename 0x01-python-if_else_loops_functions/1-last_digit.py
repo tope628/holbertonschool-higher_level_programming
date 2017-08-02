bfe5a91f09210aa8b5e6971348653093eff21d07
Tope <tope628@gmail.com>
2017-08-02 03:22:44 +0000

    neg numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,6 @@ import random
 number = random.randint(-10000, 10000)
 if number < 0:
     last = number % -10
-    last = last * -1
 else:
     last = number % 10
 print("Last digit of {:d} is {:d} and is".format(number, last), end=" ")
