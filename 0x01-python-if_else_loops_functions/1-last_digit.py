34ed9b28594b20715b9998db64c71de0a5e12184
Tope <tope628@gmail.com>
2017-08-02 03:19:42 +0000

    spaces error

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,7 +6,7 @@ if number < 0:
     last = last * -1
 else:
     last = number % 10
-print("Last digit of {:d} is {:d} and is ".format(number, last), end=" ")
+print("Last digit of {:d} is {:d} and is".format(number, last), end=" ")
 if last < 6 and last != 0:
     print("less than 6 and not 0")
 elif last == 0:
