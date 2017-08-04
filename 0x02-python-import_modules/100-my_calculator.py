9db8fc6a5be327b3c098bae0989298e7d423142e
Tope <tope628@gmail.com>
2017-08-04 05:59:26 +0000

    more errors

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
     from calculator_1 import add, sub, mul, div
@@ -11,11 +12,11 @@ if __name__ == "__main__":
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
     print("{:s} {:s} {:s} = ".format(a, op, b), end='')
-    if op == '+'
-        print("{:d}".format(add(int(a),int(b)))
-    if op == '-'
-        print("{:d}".format(sub(int(a),int(b)))
-    if op == '*'
-        print("{:d}".format(mul(int(a),int(b)))
-    if op == '/'
-        print("{:d}".format(div(int(a),int(b)))
+    if op == '+':
+        print("{:d}".format(add(int(a),int(b))))
+    if op == '-':
+        print("{:d}".format(sub(int(a),int(b))))
+    if op == '*':
+        print("{:d}".format(mul(int(a),int(b))))
+    if op == '/': 
+        print("{:d}".format(div(int(a),int(b))))
