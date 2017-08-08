9561a59cbca43d588e77c36f2f89cf4b188af338
Tope <tope628@gmail.com>
2017-08-08 22:47:29 +0000

    4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 def new_in_list(my_list, idx, element):
     copy_list = list(my_list)
     if idx > len(my_list):
-        return my_list
+        return copy_list
     else:
         copy_list.remove(copy_list[idx])
         copy_list.insert(idx, element)
