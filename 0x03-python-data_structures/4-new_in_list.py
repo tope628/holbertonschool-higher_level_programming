cdae757e1f8ff973150b658aeacaf7fea2b11f35
Tope <tope628@gmail.com>
2017-08-08 22:40:22 +0000

    first commit

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    copy_list = list(my_list)
+    if idx > len(my_list):
+        return my_list
+    else:
+        copy_list.remove(copy_list[idx])
+        copy_list.insert(idx, element)
+        return copy_list
