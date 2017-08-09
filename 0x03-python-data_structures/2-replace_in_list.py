b6dd3811ca77a8ab3171db686ed5e098ae3255c8
Tope <tope628@gmail.com>
2017-08-09 01:26:34 +0000

    bunch of errors

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if idx > len(my_list):
+    if idx > len(my_list) or idx < 0:
         return my_list
     else:
         my_list.remove(my_list[idx])
--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     copy_list = list(my_list)
-    if idx > len(my_list):
+    if idx > len(my_list) or idx < 0:
         return copy_list
     else:
         copy_list.remove(copy_list[idx])
