f1ccd0140b227d055de4b9cdc0ca719b79d121e8
Tope <tope628@gmail.com>
2017-08-09 04:28:59 +0000

    5

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    if my_string is None:
+        return None
+    my_list = [ i for i in my_string if i != 'c' and i != 'C']
+    copy_string = "".join(my_list)
+    return copy_string
