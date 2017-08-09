904912de98c5db60d5682b5eab4dcb5459629837
Tope <tope628@gmail.com>
2017-08-09 04:53:03 +0000

    10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    tf_list = []
+    if my_list is None:
+        return None
+    for i in my_list:
+        if i % 2 == 0:
+            tf_list.append(True)
+        else:
+            tf_list.append(False)
+    return tf_list
--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     if my_string is None:
         return None
-    my_list = [ i for i in my_string if i != 'c' and i != 'C']
+    my_list = [i for i in my_string if i != 'c' and i != 'C']
     copy_string = "".join(my_list)
     return copy_string
