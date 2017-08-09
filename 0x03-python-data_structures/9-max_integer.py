47642ab6922600963eede5049dbf4f67a0d87209
Tope <tope628@gmail.com>
2017-08-09 01:32:02 +0000

    errors

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if len(my_list) == 0 or my_list == None:
+    if len(my_list) == 0 or my_list is None:
         return None
     max_num = 0
     for i in my_list:
