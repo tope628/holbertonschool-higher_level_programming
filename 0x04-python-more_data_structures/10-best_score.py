137f95d4da425b2b44ae04bf4b3be781e3c018ad
Tope <tope628@gmail.com>
2017-08-11 18:50:42 +0000

    10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,10 +2,6 @@
 def best_score(my_dict):
     if my_dict is None:
         return None
-    new_tup = (my_dict.items())
-    for k, v in my_dict.items():
-        max_score = 0
-        if v > max_score:
-            max_score = v
+    new_tup = my_dict.items()
     new_list = list(max(new_tup))
     return new_list[0]
