a226643a175e5c51e1d852976e060f7290cb8202
Tope <tope628@gmail.com>
2017-08-29 06:44:11 +0000

    no checker

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width * 2) + (self.__height * 2)
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        if self.__height == 0 or self.__width == 0:
+            return ("")
+        s = ''.join(('#' * self.__width + '\n') * self.__height)
+        return s
+
+    def __repr__(self):
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
