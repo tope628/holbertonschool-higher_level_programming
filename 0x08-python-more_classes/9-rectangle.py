8b50026764e905608a4c05186f4bc83c59710038
Tope <tope628@gmail.com>
2017-08-30 22:28:07 +0000

    set variables in correct order

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -5,10 +5,22 @@ class Rectangle:
     print_symbol = "#"
 
     def __init__(self, width=0, height=0):
-        self.height = height
         self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
-        Rectangle.print_symbol = "#"
+        Rectangle.print_symbol = self.print_symbol
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):	
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
     @property
     def height(self):
@@ -21,18 +33,6 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-
-    @property
-    def width(self):
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        if type(value) != int:
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
 
     def perimeter(self):
         if self.__width == 0 or self.__height == 0:
