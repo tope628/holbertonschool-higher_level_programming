1bdd5951d894e42d28ce9677407498928fc07621
Tope <tope628@gmail.com>
2017-08-30 22:00:27 +0000

    7

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -2,13 +2,13 @@
 class Rectangle:
 
     number_of_instances = 0
-    print_symbol = "#"
+    print_symbol = '#'
 
     def __init__(self, width=0, height=0):
         self.height = height
         self.width = width
         Rectangle.number_of_instances += 1
-        Rectangle.print_symbol = "#"
+        Rectangle.print_symbol = self.print_symbol
 
     @property
     def height(self):
