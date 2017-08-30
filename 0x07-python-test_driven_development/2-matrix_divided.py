6e35d8f4d0b340d915b95cd7e732dbf3d9ebce59
Tope <tope628@gmail.com>
2017-08-30 20:38:27 +0000

    2

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -15,15 +15,15 @@ def matrix_divided(matrix, div):
          div (int): The second parameter.
 
      Returns:
-         list: All elements of matrix divided by div and original
-	 matrix the same.
+         list: All elements of matrix divided by div
+         and original matrix the same.
     """
     new_list = []
     if type(matrix) != list:
             raise TypeError("matrix must be a matrix (list of lists)\
  of integers/floats")
     if type(div) != int and type(div) != float:
-         raise TypeError("div must be a number")
+        raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
     equal_len = len(matrix[0])
