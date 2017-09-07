f8ec5e41623a5a9186242363d5417a99cd231b5e
Tope <tope628@gmail.com>
2017-09-07 05:47:31 +0000

    docs

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 def lookup(obj):
+     """
+     Args:
+         obj (object): The first parameter.
+
+     Returns:
+         list: list of available attributes and methods of an object.
+    """
     my_list = dir(obj)
     return my_list 
--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+"""MyList  Module
+"""
 class MyList(list):
+""" class that inherits ffrom list object """
 	
     def print_sorted(self):
+     """
+     Args:
+         self (object): The first parameter.
+
+     Returns:
+         list: sorted list.
+    """
         print(sorted(self))
