63b79c2bebb6d2d77ca77eb3f269f26e9c0beb66
Tope <tope628@gmail.com>
2017-09-07 06:17:21 +0000

    docs

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python3
+"""Square Module
+"""
 Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
+	"""square class"""
     def __init__(self, size):
+     """
+     Args:
+         self (object): The first parameter.
+         size (integer): The second parameter.
+     """
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
 
     def area(self):
+     """
+     Args:
+         self (object): The first parameter.
+     """
         return self.__size ** 2
--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,14 +1,31 @@
 #!/usr/bin/python3
+"""Rectangle  Module
+"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 class Rectangle(BaseGeometry):
+"""Rectangle class"""
     def __init__(self, width, height):
+    """
+     Args:
+         self (object): The first parameter.
+         width (integer): The second parameter.
+         height (integer): The third parameter.
+    """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
 
     def area(self):
+ """
+     Args:
+         self (object): The first parameter.
+"""
         return self.__width * self.__height
 
     def __str__(self):
+    """
+     Args:
+         self (object): The first parameter.
+    """
         return ("[Rectangle] {:d}/{:d}".format(self.__width, self.__height))
