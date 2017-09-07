e7ebd5a7d17ede5e34c2bdf3a37b18f3f00969d8
Tope <tope628@gmail.com>
2017-09-07 06:19:58 +0000

    docs

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,13 +1,29 @@
 #!/usr/bin/python3
+"""Square Module
+"""
 Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
+"""square class"""
     def __init__(self, size):
+    """
+     Args:
+         self (object): The first parameter.
+         size (integer): The second parameter.
+     """
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
 
     def area(self):
+     """
+     Args:
+         self (object): The first parameter.
+     """
         return self.__size ** 2
 
     def __str__(self):
+    """
+     Args:
+         self (object): The first parameter.
+     """
         return ("[Square] {:d}/{:d}".format(self.__size, self.__size))
