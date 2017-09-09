05b56ca5ce58de120de1a58ebbffc143e58dbe7f
Tope <tope628@gmail.com>
2017-09-09 22:39:06 +0000

    using super

---
+++ 0x0A-python-inheritance/100-main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+MyInt = __import__('100-my_int').MyInt
+
+my_i = MyInt(3)
+print(my_i)
+print(my_i == 3)
+print(my_i != 3)
+
+m = MyInt(0)
+print(m) 
+print(m == 0) 
+print(m != 0)
