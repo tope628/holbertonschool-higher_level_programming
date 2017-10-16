bc8e5e782796de6068d0ce5447557046f5d3cbfa
Tope <tope628@gmail.com>
2017-10-16 04:40:45 +0000

    specified state id to grab

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- because Batch 3 is the best!
-SELECT id, name FROM cities ORDER BY cities.id ASC;
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = "California") ORDER BY cities.id ASC;
