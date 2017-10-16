d6dd8346d88445104c8825ad2f851a756645c9de
Tope <tope628@gmail.com>
2017-10-16 04:51:47 +0000

    join statement used

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- because Batch 3 is the best!
-SELECT cities.id, cities.name, states.name FROM cities, states ORDER BY cities.id ASC;
+SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id=states.id ORDER BY cities.id ASC;
