076d5ef48d5ed433c87f0bdc2601510e3f08f5c3
Tope <tope628@gmail.com>
2017-11-30 19:22:48 +0000

    pep8

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,23 +3,21 @@
 
 
 def find_peak(list_of_integers):
-	"""find peak function"""
-	if len(list_of_integers) == 0:
-		return None
-	if len(list_of_integers) == 1:
-		return list_of_integers[0]
-	mid = len(list_of_integers)//2
-	peak = list_of_integers[mid]
+    """find peak function"""
+    if len(list_of_integers) == 0:
+        return None
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+    mid = len(list_of_integers)//2
+    peak = list_of_integers[mid]
 
-	if ((mid == 0 or list_of_integers[mid - 1] <= peak) and
-	(mid == len(list_of_integers) - 1 or
-	list_of_integers[mid + 1] <= peak)):
-		return peak
+    if ((mid == 0 or list_of_integers[mid - 1] <= peak) and
+            (mid == len(list_of_integers) - 1 or
+                list_of_integers[mid + 1] <= peak)):
+        return peak
 
-	elif (mid != len(list_of_integers) - 1 and
-	list_of_integers[mid + 1] >= peak):
-		return find_peak(list_of_integers[mid + 1:])
-	else:
-		return find_peak(list_of_integers[:mid])
-
-
+    elif (mid != len(list_of_integers) - 1 and
+            list_of_integers[mid + 1] >= peak):
+        return find_peak(list_of_integers[mid + 1:])
+    else:
+        return find_peak(list_of_integers[:mid])
