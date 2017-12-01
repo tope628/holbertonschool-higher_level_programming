2495f69322f86ae08b975567b3169d50df10c914
Tope <tope628@gmail.com>
2017-12-01 01:31:46 +0000

    typo

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -4,11 +4,12 @@ fetches https://intranet.hbtn.io/status
 """
 import urllib.request
 from sys import argv
+import urllib.error
 
 if __name__ == "__main__":
     try:
         with urllib.request.urlopen(argv[1]) as response:
             content = response.read().decode('utf-8')
         print(content)
-    except urlib.error.HTTPError as err:
+    except urllib.error.HTTPError as err:
         print("Error code: {}".format(err.code))
