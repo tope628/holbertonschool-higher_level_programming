3db1da18ad7299e1ecc014e0f41c3409f9d21467
Tope <tope628@gmail.com>
2017-12-01 02:23:32 +0000

    pep8

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -8,5 +8,5 @@ from requests.auth import HTTPBasicAuth
 
 if __name__ == "__main__":
     req = requests.get('https://api.github.com/users/{}'.format(argv[1]),
-            auth=HTTPBasicAuth(argv[1], argv[2]))
+                       auth=HTTPBasicAuth(argv[1], argv[2]))
     print(req.json().get('id'))
