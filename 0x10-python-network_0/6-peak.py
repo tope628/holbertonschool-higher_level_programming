b8a0bfbccab518f57f1f2f554f1f023ac5cd5b9b
Tope <tope628@gmail.com>
2017-12-02 01:11:12 +0000

    docstring

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,8 +1,9 @@
 #!/bin/python3
-# finds a peak in a list of unsorted integers
+"""finds a peak in a list of unsorted integers"""
 
 
 def find_peak(list_of_integers):
+"""find peak function"""
     if len(list_of_integers) == 0:
         return None
     if len(list_of_integers) == 1:
