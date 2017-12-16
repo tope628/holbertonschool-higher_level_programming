409cd6c41d279d199911420fb5bca090dea32b1e
Tope <tope628@gmail.com>
2017-12-16 17:14:52 +0000

    semi

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,7 +3,7 @@
 const num = parseInt(process.argv[2]);
 
 function factorial (num) {
-  if (!num || num == 1) {
+  if (!num || num === 1) {
     return (1);
   }
   return (num * factorial(num - 1));
