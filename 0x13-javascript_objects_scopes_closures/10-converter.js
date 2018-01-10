61194b1cfb8fe4f61a80eefa72e333c463baba5f
Tope <tope628@gmail.com>
2018-01-10 20:18:28 +0000

    semi

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 
 exports.converter = function (base) {
-	return function myConverter(n) {
-	return n.toString(base);
-	}
+  return function myConverter (n) {
+    return n.toString(base);
+  };
 };
-
