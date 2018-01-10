af83b0822d7f9a9b64ad8884bd70464bce7e7bd1
Tope <tope628@gmail.com>
2018-01-10 11:37:39 +0000

    in  statement changed to of

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,22 +2,22 @@
 
 const request = require('request');
 
-const options = {
-  url: 'http://swapi.co/api/films'
-};
+const url = process.argv[2];
 
 function callback (error, response, body) {
   if (!error && response.statusCode === 200) {
-    const info = JSON.parse(body);
-    const character = 'https://swapi.co/api/people/18/';
+    let info = JSON.parse(body).results;
     let count = 0;
-    for (let i = 0; i < info.results.length; i++) {
-      if (info.results[i].characters.includes(character)) {
-        count++;
-      }
+    for (let i of info) {
+	for(let x of i.characters){  
+	      if (x.endsWith('18/')) {
+		count++;
+		      break;
+	      }
+    }
     }
     console.log(count);
   }
 }
 
-request(options, callback);
+request(url, callback);
