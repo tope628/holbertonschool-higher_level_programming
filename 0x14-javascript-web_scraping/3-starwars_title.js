fcaf4e8cda4f317075bebd9ef1024c298b6d1838
Tope <tope628@gmail.com>
2018-01-10 11:19:38 +0000

    testing prev version w/o using var

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,13 +2,15 @@
 
 const request = require('request');
 
-const url = 'http://swapi.co/api/films/' + process.argv[2];
+const options = {
+  url: 'http://swapi.co/api/films/' + process.argv[2]
+};
 
 function callback (error, response, body) {
   if (!error && response.statusCode === 200) {
-    var info = JSON.parse(body);
+    const info = JSON.parse(body);
     console.log(info.title);
   }
 }
 
-request.get(url, callback);
+request(options, callback);
